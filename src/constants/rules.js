e04d00a109dad4f8844e32b5a950d84596303647
Joe Danziger <joe@ticc.net>
2025-05-23 20:09:32 -0400

    update instructions

--- src/constants/rules.js
+++ src/constants/rules.js
@@ -4,12 +4,19 @@
 
 /**
  * Available brand rules for project initialization
+ * 
  * @type {BrandRule[]}
  * @description Defines possible brand rule sets:
  * - cursor: Cursor IDE rules (default)
  * - roo: Roo Code IDE rules
  * - windsurf: Windsurf IDE rules  
  * - cline: Cline IDE rules
+ * 
+ * To add a new brand:
+ * 1. Add the brand name to this array
+ * 2. Create a profile file in scripts/profiles/{brand}.js
+ * 3. Export it in scripts/profiles/index.js
+ * 4. Add it to BRAND_PROFILES in src/utils/rule-transformer.js
  */
 export const BRAND_RULE_OPTIONS = [
 	'cursor',
